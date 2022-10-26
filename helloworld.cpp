6e24f666cc224947a6e6b035653e7269596fd855
MaZeTao2020151046 <280642604@qq.com>
2022-10-26 21:48:40 +0800

    这是我们第二个功能的实现

--- helloworld.cpp
+++ helloworld.cpp
@@ -4,6 +4,10 @@
 using namespace std;
 int main(){
 	String name="马泽涛";
+	String gender="男";
 	String Sid="2020151046";
+	String address="冬筑1424";
 	cout<<"helloWorld"<<endl;
+	cout<<"我是一名程序员."<<endl;
+          
 }
